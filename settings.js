4f2ed95f11c9623efc499e815cd8c4eb266ff88b
Amour9 <amourr822@gmail.com>
2025-06-04 14:31:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: 'Bot‎',
   botName: "Knight Bot",
-  botOwner: 'The lastson', // Your name
+  botOwner: 'The last son😎', // Your name
   ownerNumber: '255715457053', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
